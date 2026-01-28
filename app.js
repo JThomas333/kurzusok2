722565d99c707b782df1bfabaa8c6350ab00d4af
Tamás Józsa <c0joztam@vasvari.org>
2026-01-28 12:07:49 +0100

    database

---
+++ app.js
@@ -0,0 +1,28 @@
+import  express  from "express";
+import  db from "./data/db.js";
+
+const app = express();
+const PORT = 3030;
+app.use(express.json());
+
+app.get("/students", (req, res) => {
+  const clss = req.query.class;
+  const students = db.prepare("SELECT * FROM students where classes == ?").all(clss);
+  res.status(200).json(students);
+});
+
+// app.get("/students/:id", (req, res) => {
+//   const clss = req.credit.class 
+//    const students = db.prepare("SELECT * FROM students WHERE id = ?").get(req.params.id);
+  
+//   const student = db
+
+//   
+// });
+
+
+
+
+app.listen(PORT, () => {
+  console.log("run"+PORT);
+});
